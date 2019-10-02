4926b81a3c14a00ece1e33ed18f070a9bea46a80
AJMitev <mail@aleksandar.it>
2019-10-02 23:20:21 +0300

    Update model

--- CarPriceEstimator.DataGatherer/Car.cs
+++ CarPriceEstimator.DataGatherer/Car.cs
@@ -5,10 +5,10 @@
         public string Make { get; set; }
         public string Model { get; set; }
         public string FuelType { get; set; }
-        public int CubicCapacity { get; set; }
         public string GearType { get; set; }
         public int HorsePower { get; set; }
         public int Range { get; set; }
         public string Year { get; set; }
+        public int Price { get; set; }
     }
 }
